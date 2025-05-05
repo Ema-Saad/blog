ff8d49b77b50a668685f3ac3b5cebccc0b933076
Ema-Saad <seman1037@gmail.com>
2025-05-05 14:41:15 +0300

    blog was created

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import *
+
+class BlogListView(ListView):
+    model = Post
+    template_name= 'home.html'
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
